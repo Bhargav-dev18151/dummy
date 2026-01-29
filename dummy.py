c11d2d2a8ccecce2a81bb05d88dfeb922896f6fd
Bhargav-dev18151 <kollatibhargav@gmail.com>
2026-01-29 14:28:03 +0530

    dummy.py modified

--- dummy.py
+++ dummy.py
@@ -3,3 +3,14 @@ def add(num1 , num2):
     return num3
 
 a = add(150 +250)
+print(a)
+
+
+def even(num):
+    if num % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+b = add(2)
+print(b)
